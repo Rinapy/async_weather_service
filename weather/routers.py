10271092ffe82833514cf1e8bed6cb75b1b8d929
Roman Minaev <romario123321@yandex.ru>
2023-12-11 20:18:35 +0300

    add routers

---
+++ weather/routers.py
@@ -0,0 +1,7 @@
+from .views import frontend
+from aiohttp import web
+
+def setup_routers(app: web.Application) -> None:
+    '''Routers setup in app.'''
+    
+    app.add_routes([web.get('/', frontend.index)])
