c49c565745082b8ee2252552089ba573db53a3b2
Roman Minaev <romario123321@yandex.ru>
2023-12-12 23:24:15 +0300

    add future

--- external_api/settings.py
+++ external_api/settings.py
@@ -7,12 +7,19 @@ TOKEN = os.getenv('TOMORROWIO_TOKEN')
 
 DEFAULT_CORDS = '55.7522,37.6156'
 
-URL = 'https://api.tomorrow.io/v4/weather/forecast?location={cords}&timesteps=1d&apikey={TOKEN}'
+URL = 'https://api.tomorrow.io/v4/weather/forecast?location={coord}&timesteps=1m&apikey={TOKEN}'
 
 EXAMPLE_DICT = {
   'Moscow': '55.7522,37.6156',
   '...': '...'
 }
 EXAMPLE_RESPONSE_JSON = {
-  'Moscow': (37.)
+  'Moscow': (37, 23, '...'),
+  'Another_city': ('another_temp')
+}
+
+CITIES = {
+  'Moscow': '55.7522,37.6156',
+  'Ufa': '54.7431,55.9678',
+  'St-Petersburg': '59.9386,30.3141'
 }
